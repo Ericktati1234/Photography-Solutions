765b84993e567496f7d27d2e1805f2359f402ee6
CeciliaHC <cecilia.holguin44@outlook.es>
2025-02-28 18:36:56 -0600

    Almacen y Ventas

---
+++ Photography Solutions/Ventas.vb
@@ -0,0 +1,9 @@
+﻿Public Class Ventas
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Totallbl.Click
+
+    End Sub
+End Class
