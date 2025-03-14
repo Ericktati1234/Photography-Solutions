f354806395db66ff5dae433e29be6d2a2cb0877a
Ericktati1234 <a357664@uach.mx>
2025-03-14 14:09:53 -0600

    Solamente modifique el home para comunicarme con la base de datos de manera local, ademas puse un label de bienvenida en el inicio de sesion

--- Photography Solutions/Home.vb
+++ Photography Solutions/Home.vb
@@ -5,7 +5,7 @@ Public Class Home
 
     Private Sub Home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         sqlConexion = New SqlClient.SqlConnection
-        sqlConexion.ConnectionString = "workstation id = " & Dns.GetHostName & "; packet size = 8192; user id = AdminPS; data source = 10.20.11.137,1433; persist security info = False; initial catalog = photography_solutions; password= Admin123!; pooling =FALSE"
+        sqlConexion.ConnectionString = "workstation id = " & Dns.GetHostName & "; packet size = 8192; user id = AdminPS; data source = localhost,1433; persist security info = False; initial catalog = photography_solutions; password= Admin123!; pooling =FALSE"
         sqlConexion.Open()
         Dim sqlcmdComando As New SqlClient.SqlCommand
         sqlcmdComando.CommandText = "select name from clients"
--- Photography Solutions/IniciarSesión.vb
+++ Photography Solutions/IniciarSesión.vb
@@ -7,19 +7,7 @@
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Correo.TextChanged
-
-    End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles Contraseña.TextChanged
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LCorreo.Click
-
-    End Sub
-
-    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles LContraseña.Click
+    Private Sub Ingresar_Click(sender As Object, e As EventArgs) Handles Ingresar.Click
 
     End Sub
 End Class
